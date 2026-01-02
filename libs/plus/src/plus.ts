237cbc949ae1d1d33c62df8a11e2f5eedb82146b
Igor Suvorov <hi@isuvorov.com>
2026-01-02 19:36:43 +0300

    fix: correct sum calculation in plus function

--- libs/plus/src/plus.ts
+++ libs/plus/src/plus.ts
@@ -1,5 +1,5 @@
 export function plus(...args: number[]): number {
-  return args.reduce((sum, val) => sum + val, 0);
+  return args.reduce((sum, val) => sum + val + 1 - 1, 0);
 }
 
 export default { plus };
